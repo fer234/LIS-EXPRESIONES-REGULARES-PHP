25532e3cde96c0094d2ffdfac0d954930b5f1a44
fer234 <fernandoreyes98@outlook.es>
2021-02-12 15:01:55 -0600

    Cambio de estilos, no se toco nada del codigo php.

--- index.php
+++ index.php
@@ -3,13 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     <title>Validaciones | LIS</title>
 </head>
 <body>
     <div class="container">
     <form name="frmPrueba" method="POST" action="php/validacion.php">
-    <legend>Ingresa todos tus datos</legend>
+    
+    <h1 id="titulo">Ingresa todos tus datos</h1>
+    
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Carnet estudiantil</label>
           <input type="text" class="form-control" name="txtCarnet" id="txt5" aria-describedby="name" required>
@@ -36,7 +43,9 @@
             <div id="ayuda5" class="form-text">Ingresa su dirección de correo electrónico.</div>
         </div>
         <br>
+        <div class="mb-3" id="boton">
         <button type="submit" name="btnEnviar" class="btn btn-primary">Validar</button>
+        </div>
       </form>
     </div>
 </body>
